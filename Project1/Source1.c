e832345c11a69d7833e21f3811d3430da26056ab
ronst <ronst@DESKTOP-DVQVBV0>
2026-01-12 17:50:33 +0200

    5ewtrhdgfshdgfhgfd

---
+++ Project1/Source1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+
+    // Keeps the window open so you can see the output
+    printf("Press Enter to exit...");
+    getchar();
+
+    return 0;
+}
